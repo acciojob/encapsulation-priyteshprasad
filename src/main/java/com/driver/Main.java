4c86134db0874ef777006d8ecac8c81c0fa64747
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2023-10-13 21:15:04 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "name";
+        // The field RWOnly.name is not visibleJava(33554503)
+        obj.setName("name");
+        System.out.println(obj.getName());
+
+    }
 }
