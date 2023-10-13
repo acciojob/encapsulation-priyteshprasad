b8e890fa9888e2958a8bb998d935f2cf4d0d951d
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2023-10-13 21:15:48 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+        System.out.println("name is set");
+    }
 }
